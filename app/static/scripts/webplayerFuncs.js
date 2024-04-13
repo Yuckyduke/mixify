f2c86b9beb90cc303856389f1abdb510b45bf8b4
Matt Brown <mrmatthewlewisbrown@gmail.com>
2024-04-13 13:04:23 -0700

    added script directory

---
+++ app/static/scripts/webplayerFuncs.js
@@ -0,0 +1,19 @@
+function updateSeek(msTime) {
+    return (trackPosition/songDuration) * 100;
+  }
+
+  function timeConverter(msTime){
+    seconds = Math.floor((msTime % 60000)/1000);
+    minutes = Math.floor(msTime/60000);
+    if (seconds < 10){
+      return minutes.toString() + ":0" + seconds.toString();}
+    else{
+    return minutes.toString() + ":" + seconds.toString();}
+  }
+  function addASecond(){
+    trackPosition = trackPosition + 1000
+    document.getElementById("current-time").textContent = !isNaN(trackPosition) 
+    ? timeConverter(trackPosition) : "00:00";
+    document.getElementById("songSeek").value = (trackPosition/trackDuration) * 100;
+    console.log(trackPosition);
+  }
